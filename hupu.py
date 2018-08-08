cc3dfe2f1eacd2cf9357cb90e1991a3f6e96bef3
broholens <broholens@github.com>
2018-08-08 16:12:47 +0800

    sleep to tomorrow instead of exit when up to the limit

--- hupu.py
+++ hupu.py
@@ -1,38 +1,27 @@
 import time
 import logging
+import datetime
 from random import choice
 from queue import Queue
 import fire
+from config import hupu
 from utils import send_mail, load_cookies, request, config_log, config_driver
 
 
 class HuPu:
 
-    # 本日回帖数量
-    comment_count = 0
-
-    commentary = [
-        '回收各种球鞋 aj 喷泡 椰子 实战 急用鞋换钱 闲置清理空间 全新二手皆可 打包优先 寻求多方合作 更多精彩尽在: clpro7',
-        '朋友圈每日更新 各种秒价第一时间了解:clpro7',
-        '最大限度发挥球鞋价值 接各种套现寄卖 全新二手都可以 加微信:clpro7 秒价实时更新',
-        '加微信:clpro7 ',
-        '接各种套现寄卖 全新二手都可以',
-        '寻求多方合作',
-        '回收各种球鞋',
-        '急用鞋换钱 闲置清理空间 全新二手皆可'
-    ]
-
-    user_id = '131348617043133'
-
-    post_count = 0
-
     def __init__(self, username):
         self.driver = config_driver()
+        self.post_count = hupu['posts_count']
+        self.user_id = hupu['user_id']
+        self.commentaries = hupu['commentaries']
+        self.comment_count = 0
         self.posts = Queue()
         self.max_error_num = 5
         self.username = str(username)
-        config_log(self.username)
-        load_cookies(self.driver, self.username)
+        self.name = 'hupu_' + self.username
+        config_log(self.name)
+        load_cookies(self.driver, self.name)
         self.comment_posts()
 
     def retry(self):
@@ -71,7 +60,7 @@ class HuPu:
             self.retry()
 
     def request(self, url):
-        request(self.driver, url, self.username)
+        request(self.driver, url, self.name)
 
     def comment_post(self, url, commentary):
         """添加评论"""
@@ -87,7 +76,7 @@ class HuPu:
                 self.store_posts()
             post_url = self.posts.get()
             try:
-                self.comment_post(post_url, self.commentary)
+                self.comment_post(post_url, self.commentaries)
             except:
                 logging.error('find element error %s', post_url)
                 if element_error_counter > self.max_error_num:
@@ -107,21 +96,30 @@ class HuPu:
                 if '银行总资产少于' in failed_reason:
                     logging.info('sending mail...')
                     send_mail(failed_reason)
-                    exit(0)
+                    time.sleep(3 * 60 * 60)
+                elif '上限' in failed_reason:
+                    logging.info('sending mail...')
+                    send_mail(failed_reason)
+                    time.sleep((24 - datetime.time().hour) * 60 * 60)
                 else:
                     if comment_error_counter > self.max_error_num:
                         logging.info('sending mail...')
                         send_mail(f'连续回帖{self.max_error_num}次出错')
-                        exit(0)
+                        self.retry()
                     else:
                         comment_error_counter += 1
             elif '-last.html#o' in self.driver.current_url:
                 self.comment_count += 1
-                logging.info('评论 %s 成功!　已评论: %s', post_url, self.comment_count)
+                logging.info('评论 %s 成功!　累计评论: %s', post_url, self.comment_count)
                 comment_error_counter = 0
             else:
                 logging.warning('unknown state %s', self.driver.current_url)
-                comment_error_counter += 1
+                if comment_error_counter > self.max_error_num:
+                    logging.info('sending mail...')
+                    send_mail('unknown state too many times!')
+                    self.retry()
+                else:
+                    comment_error_counter += 1
 
             time.sleep(60 * 3)
 
