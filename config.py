640a676fc39a26ac60c0277d8e224fae62c1ea21
broholens <broholens@github.com>
2018-08-08 16:13:50 +0800

    config email and so on

---
+++ config.py
@@ -0,0 +1,42 @@
+hupu = {
+    'commentaries': {
+        '17121319220': [
+            '回收各种球鞋 aj 喷泡 椰子 实战 急用鞋换钱 闲置清理空间 全新二手皆可 打包优先 寻求多方合作 更多精彩尽在: clpro7',
+            '朋友圈每日更新 各种秒价第一时间了解:clpro7',
+            '最大限度发挥球鞋价值 接各种套现寄卖 全新二手都可以 加微信:clpro7 秒价实时更新',
+            '加微信:clpro7 ',
+            '接各种套现寄卖 全新二手都可以',
+            '寻求多方合作',
+            '回收各种球鞋',
+            '急用鞋换钱 闲置清理空间 全新二手皆可'
+        ],
+        '17121319091': [
+            '回收各种球鞋 aj 喷泡 椰子 实战 急用鞋换钱 闲置清理空间 全新二手皆可 打包优先 寻求多方合作 更多精彩尽在: clpro7',
+            '朋友圈每日更新 各种秒价第一时间了解:clpro7',
+            '最大限度发挥球鞋价值 接各种套现寄卖 全新二手都可以 加微信:clpro7 秒价实时更新',
+            '加微信:clpro7 ',
+            '接各种套现寄卖 全新二手都可以',
+            '寻求多方合作',
+            '回收各种球鞋',
+            '急用鞋换钱 闲置清理空间 全新二手皆可'
+        ]
+    },
+    'user_id': '131348617043133',
+    'posts_count': 0
+}
+
+douban = {
+    'posts': {
+        '18717244229': [
+            'https://www.douban.com/group/topic/120671545/',
+            'https://www.douban.com/group/topic/120671526/',
+            'https://www.douban.com/group/topic/120671489/',
+            'https://www.douban.com/group/topic/120671432/',
+            'https://www.douban.com/group/topic/120674005/',
+            'https://www.douban.com/group/topic/120671432/',
+            'https://www.douban.com/group/topic/120670949/',
+        ]
+    }
+}
+
+email = 'zzwcng@126.com'
